6f65fbe89be3a040d38e69742159791cd7a05f57
Yuvaraj Chesetti <karch08@gmail.com>
2018-10-03 21:15:20 +0530

    Rotation of pieces.

--- src/main.rs
+++ src/main.rs
@@ -26,60 +26,118 @@ trait Drawable {
     fn draw<T:RenderTarget>(&self, canvas: &mut Canvas<T>, pos:Pos2D);
 }
 
-struct TetrisPiece {
-    shape: [Pos2D; 4],
 
+struct TetrisPiece {
+    pos: Pos2D,
+    shape: [[Pos2D; 4]; 4],
+    orientation: usize, 
     // Drawing Related Constants
     color: Color,
 }
 
+
 impl TetrisPiece {
-    fn build_stick_piece() -> Self {
+    fn build_stick_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(3,0) ],
+            pos: pos,
+            shape: [
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0) ],
+                [ Pos2D::xy(1,-1), Pos2D::xy(1,0), Pos2D::xy(1,1), Pos2D::xy(1,2) ],
+                [ Pos2D::xy(-1,1), Pos2D::xy(0,1), Pos2D::xy(1,1), Pos2D::xy(2,1) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(0,2) ],
+            ],
             color: Color::RGB(0, 255, 255),
+            orientation: 0usize,
         }
     }
-    fn build_block_piece() -> Self {
+
+    fn build_block_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(1,1), Pos2D::xy(0,1) ],
+            pos: pos,
+            shape: [
+                [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(1,1), Pos2D::xy(0,1) ],
+                [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(3,0) ],
+                [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(3,0) ],
+                [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(3,0) ],
+            ],
             color: Color::RGB(255, 255, 0),
+            orientation: 0usize,
         }
     }
 
-    fn build_t_piece() -> Self {
+    fn build_t_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(1,0), Pos2D::xy(0,1), Pos2D::xy(1,1), Pos2D::xy(2,1) ],
+            pos: pos,
+            shape: [
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(-1,0) ],
+                [ Pos2D::xy(1,0), Pos2D::xy(0,1), Pos2D::xy(0,-1), Pos2D::xy(0,0) ],
+                [ Pos2D::xy(0,1), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(-1,0) ],
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,1), Pos2D::xy(0,-1), Pos2D::xy(0,0) ],
+            ],
             color: Color::RGB(128, 0, 128),
+            orientation: 0usize,
         }
     }
 
-    fn build_s_piece() -> Self {
+    fn build_s_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(1,1), Pos2D::xy(0,1) ],
+            pos: pos,
+            shape: [
+                [ Pos2D::xy(1,-1), Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(-1,0) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(1,1) ],
+                [ Pos2D::xy(1,0), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(-1,1) ],
+                [ Pos2D::xy(-1,-1), Pos2D::xy(0,0), Pos2D::xy(-1,0), Pos2D::xy(0,1) ],
+            ],
+
             color: Color::RGB(0, 255, 0),
+            orientation: 0usize,
         }
     }
 
-    fn build_z_piece() -> Self {
+    fn build_z_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(1,1), Pos2D::xy(2,1) ],
+            pos: pos,
+            shape: [ 
+                [ Pos2D::xy(-1,-1), Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(1,0) ],
+                [ Pos2D::xy(1,-1), Pos2D::xy(1,0), Pos2D::xy(0,0), Pos2D::xy(0,1) ],
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(1,1) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(-1,0), Pos2D::xy(-1,1) ],
+                ],
             color: Color::RGB(255, 0, 0),
+            orientation: 0usize,
         }
     }
 
-    fn build_j_piece() -> Self {
+    fn build_j_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(2,1) ],
+            pos: pos,
+            shape:[ 
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(-1,-1) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(1,-1) ],
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(1,1) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(-1,1) ],
+            ],
             color: Color::RGB(0, 0, 255),
+            orientation: 0usize,
         }
     }
 
-    fn build_l_piece() -> Self {
+    fn build_l_piece(pos: Pos2D) -> Self {
         TetrisPiece {
-            shape: [ Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(2,0), Pos2D::xy(0,1) ],
+            pos: pos,
+            shape: [
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(1,-1) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(1,1) ],
+                [ Pos2D::xy(-1,0), Pos2D::xy(0,0), Pos2D::xy(1,0), Pos2D::xy(-1,1) ],
+                [ Pos2D::xy(0,-1), Pos2D::xy(0,0), Pos2D::xy(0,1), Pos2D::xy(-1,-1) ],
+            ],
             color: Color::RGB(255, 165, 0),
+            orientation: 0usize,
         }
+    }
+
+    fn rotate(&mut self) {
+        self.orientation = (self.orientation + 1) % 4;
     }
 }
 
@@ -87,7 +145,7 @@ impl Drawable for TetrisPiece {
     fn draw<T:RenderTarget>(&self, canvas: &mut Canvas<T>, pos:Pos2D) {
         let box_width = 20;
         canvas.set_draw_color(self.color);
-        for diff in self.shape.iter() {
+        for diff in self.shape[self.orientation].iter() {
             let rect = Rect::new(
                 pos.x + diff.x * box_width, 
                 pos.y + diff.y * box_width, 
@@ -100,8 +158,8 @@ impl Drawable for TetrisPiece {
 }
 
 fn main() {
-    let width = 800;
-    let height = 600;
+    let width = 200;
+    let height = 400;
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -115,13 +173,13 @@ fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let stick_piece = TetrisPiece::build_stick_piece();
-    let box_piece = TetrisPiece::build_block_piece();
-    let t_piece = TetrisPiece::build_t_piece();
-    let s_piece = TetrisPiece::build_s_piece();
-    let z_piece = TetrisPiece::build_z_piece();
-    let j_piece = TetrisPiece::build_j_piece();
-    let l_piece = TetrisPiece::build_l_piece();
+    let mut stick_piece = TetrisPiece::build_stick_piece(Pos2D::xy(0,0));
+    let box_piece = TetrisPiece::build_block_piece(Pos2D::xy(0,0));
+    let mut t_piece = TetrisPiece::build_t_piece(Pos2D::xy(0,0));
+    let mut s_piece = TetrisPiece::build_s_piece(Pos2D::xy(0,0));
+    let mut z_piece = TetrisPiece::build_z_piece(Pos2D::xy(0,0));
+    let mut j_piece = TetrisPiece::build_j_piece(Pos2D::xy(0,0));
+    let mut l_piece = TetrisPiece::build_l_piece(Pos2D::xy(0,0));
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -132,15 +190,26 @@ fn main() {
                 _ => {}
             }
         }
+
+        canvas.set_draw_color(Color::RGB(0,0,0));
+        canvas.fill_rect(Rect::new(0,0,width,height));
+
         stick_piece.draw(&mut canvas, Pos2D::xy(30, 50));
-        box_piece.draw(&mut canvas, Pos2D::xy(100, 100));
-        t_piece.draw(&mut canvas, Pos2D::xy(180, 100));
-        s_piece.draw(&mut canvas, Pos2D::xy(280, 100));
-        z_piece.draw(&mut canvas, Pos2D::xy(380, 100));
-        j_piece.draw(&mut canvas, Pos2D::xy(180, 200));
-        l_piece.draw(&mut canvas, Pos2D::xy(280, 200));
+        box_piece.draw(&mut canvas, Pos2D::xy(150, 100));
+        t_piece.draw(&mut canvas, Pos2D::xy(80, 100));
+        s_piece.draw(&mut canvas, Pos2D::xy(80, 180));
+        z_piece.draw(&mut canvas, Pos2D::xy(20, 180));
+        j_piece.draw(&mut canvas, Pos2D::xy(80, 320));
+        l_piece.draw(&mut canvas, Pos2D::xy(80, 240));
+
+        stick_piece.rotate();
+        t_piece.rotate();
+        s_piece.rotate();
+        z_piece.rotate();
+        j_piece.rotate();
+        l_piece.rotate();
 
         canvas.present();
-        thread::sleep(Duration::new(0, 1_000_000u32 / 60));
+        thread::sleep(Duration::new(2, 0));
     }
 }
